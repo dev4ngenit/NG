dbb79666b6bad5e4a4d14ab9d49c298baf665608
dev4ngenit <112759197+dev4ngenit@users.noreply.github.com>
2022-10-05 15:57:49 +0600

    update js cliend dasboard

--- ngenit/js/jquery.mr.js
+++ ngenit/js/jquery.mr.js
@@ -57,7 +57,32 @@ $(".toggle").on("click", function(e){
 */
 //-------------
 
+// $('#click_advance').click(function() {
+//     $('#display_advance').toggle('1000');
+//     $("i", this).toggleClass("fa-solid fa-chevron-left fa-solid fa-align-left");
+// });
+
+//-------------
+
 $('#click_advance').click(function() {
     $('#display_advance').toggle('1000');
-    $("i", this).toggleClass("fa-solid fa-chevron-left fa-solid fa-align-left");
+    // $("i", this).toggleClass("fa-solid fa-chevron-left fa-solid fa-align-left");
 });
+
+
+$(".fa-chevron-left").show();
+$(".fa-align-left").hide();
+function my(){
+    $(".fa-align-left").show();
+    $(".fa-chevron-left").hide();
+$("#cls").addClass('col-xl-12')
+}
+function my2(){
+$(".fa-chevron-left").show();
+$("#display_advance").show();
+$(".fa-align-left").hide();
+if ($( "#cls" ).hasClass('col-xl-12')) {
+$( "#cls" ).removeClass( 'col-xl-12');
+} 
+
+}
