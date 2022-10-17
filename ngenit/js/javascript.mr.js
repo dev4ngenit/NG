757befe02d21daa7a514bc1feaa907780a097281
dev4ngenit <112759197+dev4ngenit@users.noreply.github.com>
2022-10-17 17:40:55 +0600

    update

---
+++ ngenit/js/javascript.mr.js
@@ -0,0 +1,147 @@
+/*================///Cookies page ///==============*/
+//----Check Box Show Hide
+function myCheckboxbtn() {
+    var checkBox = document.getElementById("myCheckboxbtn");
+    var btn = document.getElementById("btn_show");
+    if (checkBox.checked == true) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+}
+function myCheckboxbtn1() {
+    var checkBox = document.getElementById("myCheckboxbtn1");
+    var btn = document.getElementById("btn_show");
+    if (checkBox.checked == true) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+}
+function myCheckboxbtn2() {
+    var checkBox = document.getElementById("myCheckboxbtn2");
+    var btn = document.getElementById("btn_show");
+    if (checkBox.checked == true) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+}
+//-----Cookies Details Btn
+function switchVisible() {
+    if (document.getElementById('Cookies').style.display == 'none') {
+        document.getElementById('Cookies').style.display = 'flex';
+        document.getElementById('Cookies_details').style.display = 'none';
+    }
+    else {
+        document.getElementById('Cookies').style.display = 'none';
+        document.getElementById('Cookies_details').style.display = 'flex';
+    }
+}
+/*================///Cookies page End Section///==============*/
+
+/*================///Sidebar Tab Content Change///==============*/
+//----Tab button content show hide
+function openCity(evt, cityName) {
+    var i, tab_cookises_tabcontent, tablinks;
+    tab_cookises_tabcontent = document.getElementsByClassName("tab_cookises_tabcontent");
+    for (i = 0; i < tab_cookises_tabcontent.length; i++) {
+        tab_cookises_tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+/*==============///Sidebar Tab Content Change End///=============*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------hide than show------btn(#show)----show(.menu)
+$(document).ready(function () {
+    $('#show').click(function () {
+        $('.menu').toggle("slide");
+    });
+});
+
+/*
+<div class="toggle"></div>
+
+.toggle{
+    height:48px;
+    width:48px;
+    margin-top: 20px;
+    float: left;
+    background-image:url("../images/icon/menu-icon.png");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+}
+.toggle.expanded{
+    position: absolute;
+    margin-left: 250px;
+    margin-top: 32px;
+    transition: 2s;
+    height:20px;
+    width:20px;
+    background-image:url("../images/icon/left-arrow.png");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+//-----image button hide show---div-btn(.toggle)---show(.content)
+$(document).ready(function(){
+var $content = $(".content").hide();
+$(".toggle").on("click", function(e){
+    $(this).toggleClass("expanded");
+    $content.slideToggle();
+});
+});
+*/
+//-------------
+
+// $('#click_advance').click(function() {
+//     $('#display_advance').toggle('1000');
+//     $("i", this).toggleClass("fa-solid fa-chevron-left fa-solid fa-align-left");
+// });
+
--- ngenit/js/jquery.mr.js
+++ ngenit/js/jquery.mr.js
@@ -1,88 +1,36 @@
-//---------menu----------
-$(document).ready(function(){
-    $(".accordion-heading").click(function(){
-        if($(".accordion-body").is(':visible')){
+/*================///Client Dashboard page ///==============*/
+//-------Sidebar Show Hide
+$('#click_advance').click(function () {
+    $('#display_advance').toggle('1000');
+    // $("i", this).toggleClass("fa-solid fa-chevron-left fa-solid fa-align-left");
+});
+
+$(".fa-chevron-left").show();
+$(".fa-align-left").hide();
+function my() {
+    $(".fa-align-left").show();
+    $(".fa-chevron-left").hide();
+    $("#cls").addClass('col-xl-12')
+}
+function my2() {
+    $(".fa-chevron-left").show();
+    $("#display_advance").show();
+    $(".fa-align-left").hide();
+    if ($("#cls").hasClass('col-xl-12')) {
+        $("#cls").removeClass('col-xl-12');
+    }
+
+}
+//---------Sidebar list Show Hide----------
+$(document).ready(function () {
+    $(".accordion-heading").click(function () {
+        if ($(".accordion-body").is(':visible')) {
             $(".accordion-body").slideUp(600);
             $(".plusminus").text('+')
         }
-        else{
+        else {
             $(this).next(".accordion-body").slideDown(600);
             $(this).children(".plusminus").text('-');
         }
     });
 });
-
-//-------hide than show------btn(#show)----show(.menu)
-$(document).ready(function(){
-    $('#show').click(function() {
-    $('.menu').toggle("slide");
-    });
-});
-
-/*
-<div class="toggle"></div>
-
-.toggle{
-    height:48px;
-    width:48px;
-    margin-top: 20px;
-    float: left;
-    background-image:url("../images/icon/menu-icon.png");
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    
-}
-.toggle.expanded{
-    position: absolute;
-    margin-left: 250px;
-    margin-top: 32px;
-    transition: 2s;
-    height:20px;
-    width:20px;
-    background-image:url("../images/icon/left-arrow.png");
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-}
-
-//-----image button hide show---div-btn(.toggle)---show(.content)
-$(document).ready(function(){
-var $content = $(".content").hide();
-$(".toggle").on("click", function(e){
-    $(this).toggleClass("expanded");
-    $content.slideToggle();
-});
-});
-*/
-//-------------
-
-// $('#click_advance').click(function() {
-//     $('#display_advance').toggle('1000');
-//     $("i", this).toggleClass("fa-solid fa-chevron-left fa-solid fa-align-left");
-// });
-
-//-------------
-
-$('#click_advance').click(function() {
-    $('#display_advance').toggle('1000');
-    // $("i", this).toggleClass("fa-solid fa-chevron-left fa-solid fa-align-left");
-});
-
-
-$(".fa-chevron-left").show();
-$(".fa-align-left").hide();
-function my(){
-    $(".fa-align-left").show();
-    $(".fa-chevron-left").hide();
-$("#cls").addClass('col-xl-12')
-}
-function my2(){
-$(".fa-chevron-left").show();
-$("#display_advance").show();
-$(".fa-align-left").hide();
-if ($( "#cls" ).hasClass('col-xl-12')) {
-$( "#cls" ).removeClass( 'col-xl-12');
-} 
-
-}
